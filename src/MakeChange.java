438710d9b3a28dfb4d11bcb6aaa610a7b49bce69
Mathew Smyth <smythnwesson@gmail.com>
2021-07-24 09:58:19 -0600

    Beginning of MakeChange Project

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
 
 public class MakeChange {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("What is the price of the item: ");
+		double price = sc.nextDouble();
+		System.out.println("price ");
 		
+		System.out.println("How much money was tendered by the customer: ");
+		double tendered = sc.nextDouble();
+		System.out.println(tendered);
+		
+		if(tendered < price) {
+			System.out.println("Not enough money. Please provide appropriate ammount for the purchase.");
+			
+		}
+		else if(tendered == price) {
+			System.out.println("Thank you for your transaction. No change required.");
+		}
+		else {
+			double change = tendered - price;
+			System.out.println("Please prove change for $" + change);
+		
+		}
 		
 	}
 
